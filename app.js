b5850c023f557db8c8c67194dfe172fc47fa66c9
Will <will513r@gmail.com>
2023-12-06 16:43:40 -0500

    node
    I dont like node and ejs right now

--- app.js
+++ app.js
@@ -1,18 +1,24 @@
 // app.js
+
 const express = require("express");
 const app = express();
+
 const port = 3000;
+
+app.use(express.static("public"));
 
 // Set EJS as the view engine
 app.set("view engine", "ejs");
 app.set("views", "views");
 
 // Route to render the EJS file
+
 app.get("/", (req, res) => {
-  const data = {
-    username: "William Ray", // You can pass dynamic data here
-  };
-  res.render("index", data);
+  res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
 });
 
 // Start the server
